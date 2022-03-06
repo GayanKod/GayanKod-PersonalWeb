9b1d923332f3c196812e6b3498624d91dd0d6349
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-03-07 00:11:33 +0530

    Create basic frontend folder and file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Navbar from './components/Navbar/Navbar.jsx';
+import About from './container/About/About.jsx'
+import Footer from './container/Footer/Footer.jsx'
+import Header from './container/Header/Header'
+import Skills from './container/Skills/Skills'
+import Testimonials from './container/Testimonial/Testimonials'
+import Work from './container/Work/Work'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
     </div>
   );
 }
