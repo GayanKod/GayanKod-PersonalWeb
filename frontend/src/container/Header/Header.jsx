42c4a0b212cac3917da8882b6525a624c3bdd4b4
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-03-11 01:18:40 +0530

    add id

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -16,13 +16,13 @@ const scaleVariants = {
 
 const Header = () => {
   return (
-    <div className="app__header app__flex">
+    <div id="home" className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100,0], opacity:[0,1] }}
         transition={{ duration: 0.5 }}
         className="app__header-info"
       >
-        <div className="app_header=badge">
+        <div className="app__header-badge">
             <div className="badge-cmp app__flex">
               <span>👋</span>
               <div style={{ marginLeft:20}}>
@@ -31,7 +31,7 @@ const Header = () => {
               </div>  
             </div>
 
-            <div className="tage-cmp app__flex">
+            <div className="tag-cmp app__flex">
               <p className="p-text">IT Undergraduate</p>  
               <p className="p-text">Cyber Security Enthusiast</p>
             </div>
@@ -58,7 +58,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.flutter, images.redux, images.sass].map((circle,index)=>(
+        {[images.react, images.redux, images.sass].map((circle,index)=>(
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="circle"/>
           </div>
