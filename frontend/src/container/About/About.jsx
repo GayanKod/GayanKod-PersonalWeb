3d7d87e5e8e36e671203a9a418702752c3c4d8d1
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-03-14 00:08:44 +0530

    Plug Sanity Backend to About Section

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import "./About.scss"
 import {motion} from "framer-motion"
 import {images} from "../../constants"
+import { urlFor, client } from "../../client"
 
 const abouts = [
   { 
@@ -27,6 +28,18 @@ const abouts = [
 ]
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts]'
+
+    client.fetch(query)
+      .then((data) => {
+        setAbouts(data);
+      })
+  }, []);
+  
+
   return (
     <>
       <h2 className="head-text">
@@ -43,7 +56,7 @@ const About = () => {
                 className="app__profile-item"
                 key={about.title + index}
               >
-                <img src={about.imgUrl} alt={about.title} />
+                <img src={urlFor(about.imgUrl)} alt={about.title} />
                 <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
                 <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
               </motion.div>
