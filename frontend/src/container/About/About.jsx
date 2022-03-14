6809608a2ac8367c048cde4bfde582b19f246805
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-03-15 00:29:41 +0530

    Create Slider Navigation with AppWrap

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import "./About.scss"
 import {motion} from "framer-motion"
 import { urlFor, client } from "../../client"
+import AppWrap from "../../wrapper/AppWrap"
 
 
 const About = () => {
@@ -44,4 +45,4 @@ const About = () => {
   )
 }
 
-export default About
+export default AppWrap(About, 'about');
