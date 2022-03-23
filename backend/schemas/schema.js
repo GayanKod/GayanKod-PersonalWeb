a30aef2cc38891ea4807885852b7e165ab03619d
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-03-24 00:37:20 +0530

    Add skills schema to sanity backend

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -6,6 +6,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import testimonials from './testimonials'
 import abouts from './abouts'
 import works from './works'
+import skills from './skills'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -16,6 +17,7 @@ export default createSchema({
   types: schemaTypes.concat([
     testimonials,
     abouts,
-    works
+    works,
+    skills
   ]),
 })
