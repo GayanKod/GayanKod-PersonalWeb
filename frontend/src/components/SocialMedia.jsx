c4b7b155272b7b400cda9c9f1884643811dc6c6c
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-03-25 01:49:05 +0530

    Add linkedin, codepen, github to social medais

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { BsTwitter, BsInstagram } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
+import { BsTwitter, BsInstagram, BsGithub } from "react-icons/bs";
+import { FaFacebookF, FaLinkedinIn, FaCodepen } from "react-icons/fa";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
         <div>
             <a href="https://twitter.com/gayankodX" target="_blank" rel="noreferrer"><BsTwitter /></a>
+        </div>
+        <div>
+            <a href="https://www.linkedin.com/in/gayankod/" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+        </div>
+        <div>
+            <a href="https://github.com/GayanKod" target="_blank" rel="noreferrer"><BsGithub /></a>
+        </div>
+        <div>
+            <a href="https://codepen.io/gayankod" target="_blank" rel="noreferrer"><FaCodepen /></a>
         </div>
         <div>
             <a href="https://facebook.com/gayankod" target="_blank" rel="noreferrer"><FaFacebookF /></a>
